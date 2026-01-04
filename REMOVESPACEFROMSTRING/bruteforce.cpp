7f21ee7b68f4a614de7b021d6a3852a444022869
shaunakkk <161214557+callmeshaun@users.noreply.github.com>
2026-01-04 06:25:22 +0000

    bf

---
+++ REMOVESPACEFROMSTRING/bruteforce.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string s;
+    getline(cin , s);
+
+    string result ="" ;
+
+    for(int i=0 ; i<s.length() ; i++){
+
+        if(s[i] != ' '){
+
+            result+=s[i];
+        }
+    }
+
+    cout<<result;
+
+    return 0;
+}
